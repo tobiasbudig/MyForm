80cb76f4fa69ac2754f056627e9892bee4fa2a51
Tobias Budig <tobias_b@outlook.de>
2025-12-16 18:32:46 +0100

    changes homepage content

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,33 +12,36 @@ export default function HomePage() {
     >
       <div className="max-w-2xl text-center flex-1 flex items-center">
         <div>
-          <h1 className="text-5xl font-bold text-textPrimary mb-12">
-            Willkommen zur ETH Umfrage über KI Nuztzung im medizinischen Alltag
-          </h1>
+          <h1 className="text-3xl sm:text-5xl font-bold text-textPrimary mb-12"> Willkommen</h1>
+
+          <p className="text-lg sm:text-2xl font-medium text-textPrimary mb-12">
+            Als Forschende der ETH Zürich möchten wir verstehen wie KI den Medizinischen Alltag verbessern kann! Die Umfrage dauert nur 3-4 Minuten - Wirklich!
+Vielen Dank für ihre Hilfe!
+          </p>
           <div className="flex flex-col sm:flex-row gap-6 justify-center items-center">
             <Link
               to="/form/doctor"
-              className="px-12 py-4 bg-primary text-white text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-auto"
+              className="px-12 py-4 bg-primary text-white text-base sm:text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-64 h-16 flex items-center justify-center"
             >
-              Für Ärzte
+              Ich bin Ärzt*in
             </Link>
             <Link
               to="/form/mfa"
-              className="px-12 py-4 bg-primary text-white text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-auto"
+              className="px-12 py-4 bg-primary text-white text-base sm:text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-64 h-16 flex items-center justify-center"
             >
-              Für MFAs
+              Ich bin MFA / MPA
             </Link>
             <Link
               to="/form/nurse"
-              className="px-12 py-4 bg-primary text-white text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-auto"
+              className="px-12 py-4 bg-primary text-white text-base sm:text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-64 h-16 flex items-center justify-center"
             >
-              Für Pfleger
+              Ich bin Pflegefachperson
             </Link>
             <Link
               to="/form/participant"
-              className="px-12 py-4 bg-primary text-white text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-auto"
+              className="px-12 py-4 bg-primary text-white text-base sm:text-lg rounded-lg font-medium hover:bg-primary-hover transition-default hover:scale-105 hover:shadow-lg w-full sm:w-64 h-16 flex items-center justify-center"
             >
-              Für andere Teilnehmer
+              Ich bin Privatperson
             </Link>
           </div>
         </div>
